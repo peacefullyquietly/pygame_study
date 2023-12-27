7bbad37a3a6d46924cf9397f579fa045a9755dd3
wjdsbs <wjdsbss@gmail.com>
2023-12-27 21:04:27 +0900

    [8:33]배경 출력

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import assets
+import configs
+
+from objects.background import Background
+
+pygame.init()
+
+screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+assets.load_sprites()
+sprites = pygame.sprite.LayeredUpdates()
+
+Background(sprites)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("pink")
+
+    sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(configs.FPS)
+
+pygame.quit()
---
+++ objects/background.py
@@ -0,0 +1,10 @@
+import pygame.sprite
+import assets
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        self.image = assets.get_sprite("background")
+        self.rect = self.image.get_rect(topleft=(0, 0))
+
+        super().__init__(*groups)
