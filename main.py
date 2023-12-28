701c0a4bc1295961030076f17d98f18c4a9e5985
wjdsbs <wjdsbss@gmail.com>
2023-12-29 00:30:59 +0900

    [28:35] Column sprite

--- main.py
+++ main.py
@@ -3,26 +3,37 @@ import assets
 import configs
 
 from objects.background import Background
+from objects.column import Column
+from objects.floor import Floor
 
 pygame.init()
 
 screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+column_create_event = pygame.USEREVENT
 running = True
 
 assets.load_sprites()
 sprites = pygame.sprite.LayeredUpdates()
 
-Background(sprites)
 
+Background(0, sprites)
+Background(1, sprites)
+Floor(0, sprites)
+Floor(1, sprites)
+
+pygame.time.set_timer(column_create_event, 1500)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == column_create_event:
+            Column(sprites)
 
     screen.fill("pink")
 
     sprites.draw(screen)
+    sprites.update()
 
     pygame.display.flip()
     clock.tick(configs.FPS)
---
+++ objects/floor.py
@@ -0,0 +1,18 @@
+import configs
+import assets
+import pygame.sprite
+
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, index, *groups):
+        self.image = assets.get_sprite("floor")
+        self.rect = self.image.get_rect(bottomleft=(
+            configs.SCREEN_WIDTH * index, configs.SCREEN_HEIGHT))
+
+        super().__init__(*groups)
+
+    def update(self):
+        self.rect.x -= 2
+
+        if self.rect.right <= 0:
+            self.rect.x = configs.SCREEN_WIDTH
