ec44f9435901ad0062c4ff46fa7cbaf481808716
peacefullyquietly <pjh73015@gmail.com>
2024-01-04 00:47:10 +0900

    Background sprite

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+import configs
+import assets
+from objects.background import Background
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+assets.load_sprites()
+
+sprites = pygame.sprite.LayeredUpdates()
+
+Background(0, sprites)
+Background(1, sprites)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("pink")
+
+    sprites.draw(screen)
+    sprites.update()
+
+    pygame.display.flip()
+    clock.tick(configs.FPS)
+
+pygame.quit()
